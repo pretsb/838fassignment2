1bfedc4b0881d4702a35a1de9097607933f95cb7
pretsb <prbharga@cs.umd.edu>
2012-09-27 20:28:14 -0400

    added font size change effect

--- distanceandlightSensor.java
+++ distanceandlightSensor.java
@@ -5,13 +5,27 @@ public class distanceandlightSensor {
 	public static void main(String args[]){
 	JFrame frame = new JFrame("Menu");
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setSize(800,800);	
+	frame.setSize(200,200);	
 	JPanel panel = new JPanel(); // the panel is not visible in output
 	JTextArea ta = new JTextArea();
 	panel.add(ta);
-	ta.setText("Hello");
+	
 	frame.getContentPane().add(BorderLayout.SOUTH,panel);
 	frame.getContentPane().add(BorderLayout.CENTER,ta);
 	frame.setVisible(true);
+	
+	for(int size= 16;size<32;size++){
+	try {
+		Thread.sleep(25);
+		ta.setFont(new Font("Serif", Font.ITALIC, size));
+		ta.setForeground(Color.blue);
+		ta.setText("Hello");
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	}
+	
 	}
 }
